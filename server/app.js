232e1a786367b37e710e9c59fab699aa91f17ccc
Humberto Acea <humbertoacea@gmail.com>
2017-08-23 11:37:55 +0200

    I give up

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3001
 
 app.use(express.static(path.join(__dirname, '../client')))
 
-app.get('/home', (req, res) => res.sendFile(path.join(__dirname, '../client/index.html')))
+// app.get('/home', (req, res) => res.sendFile(path.join(__dirname, '../client/index.html')))
 
 app.listen(PORT)
 console.log(`Dude PORT ${PORT} is listening.... be scare.....`)
