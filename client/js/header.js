751593ff72e2361c02089717f765c8ec7f28966a
Humberto Acea <humbertoacea@gmail.com>
2017-08-23 16:25:57 +0200

    user-page first iteration

--- client/js/header.js
+++ client/js/header.js
@@ -81,7 +81,7 @@
 function init () {
   window.addEventListener('scroll', function (e) {
     var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-      shrinkOn = 300,
+      shrinkOn = 200,
       header = document.querySelector('header')
     if (distanceY > shrinkOn) {
       classie.add(header, 'smaller')
