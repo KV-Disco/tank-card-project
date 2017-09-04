ed11f7e1d10debb86d604af4d95752fd627b6f82
Humberto Acea <humbertoacea@gmail.com>
2017-09-04 17:06:49 +0200

    On click cards on garage change to border blue

--- client/js/routes/user-page/controller.js
+++ client/js/routes/user-page/controller.js
@@ -8,11 +8,8 @@ angular.module('cardsOfKurskApp')
   			$scope.userCards = res.data
   		})
 
-      $scope.ifCardOnTrade = function (isOnTrade){
-        if(isOnTrade){return {'border-color': 'blue'}}
-      }
-
   		$scope.pushToTrade = function (userCardId) {
+        $scope.isTrading = 'onTrading'
         console.log(userCardId)
   			cardService.pushToTrade(userCardId)
   		}
