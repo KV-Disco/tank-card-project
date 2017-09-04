84d8da844917731ff22c316df9d130cc2b2f5628
Humberto Acea <humbertoacea@gmail.com>
2017-09-04 17:49:27 +0200

    The tadring park update to the user

--- client/js/routes/trading-park/controller.js
+++ client/js/routes/trading-park/controller.js
@@ -6,6 +6,8 @@ angular.module('cardsOfKurskApp')
         $scope.userCards = res.data
       })
 
+      // socket.on('updateNow', )
+
     $scope.isUserCard = function(sessionId, userCardId) {
       console.log(sessionId)
       console.log(userCardId)
@@ -22,7 +24,12 @@ angular.module('cardsOfKurskApp')
         const newUserId = res.data
         if(newUserId !== oldUserId){
           console.log(newUserId)
-          cardService.pickFromTrade(userCardId, newUserId, oldUserId)
+          cardService.pickFromTrade(userCardId, newUserId, oldUserId).then(
+            cardService.getTradingCards()
+            .then(function (res) {
+              $scope.userCards = res.data
+            // socket.emit('updateAll')
+            }))
         }
         else{
            var toast = toastr.warning('You are trying to get card that you yourself push.');
--- client/js/routes/user-page/controller.js
+++ client/js/routes/user-page/controller.js
@@ -2,16 +2,28 @@
 
 angular.module('cardsOfKurskApp')
   .controller('userController', function ($scope, $rootScope, cardService) {
+
+    $scope.isTrading = {}
+
   	cardService.getUserCards()
   		.then(function (res) {
   			console.log(res)
   			$scope.userCards = res.data
+
+        // TODO for loop over user cards
+        $scope.isTrading = userCard.isTrading
   		})
 
+      $scope.ifCardOnTrade = function (isOnTrade){
+        if(isOnTrade){return {'border-color': 'blue'}}
+      }
+
   		$scope.pushToTrade = function (userCardId) {
-        $scope.isTrading = 'onTrading'
         console.log(userCardId)
   			cardService.pushToTrade(userCardId)
+          .then(result => {
+            $scope.isTrading[userCardId] = true
+          })
   		}
   })
   
