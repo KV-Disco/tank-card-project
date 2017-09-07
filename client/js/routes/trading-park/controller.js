40b3f3dc770633476e5e5cc9eff1ade7dc73e988
Humberto Acea <humbertoacea@gmail.com>
2017-09-07 22:20:42 +0200

    loading cards

--- client/js/routes/trading-park/controller.js
+++ client/js/routes/trading-park/controller.js
@@ -3,7 +3,6 @@ angular.module('cardsOfKurskApp')
   .controller('tradingController', function ($scope, $rootScope, cardService, userService, toastr) {
     var socket = io();
     $scope.itIs = false
-
     cardService.getTradingCards()
       .then(function (res) {
         $scope.userCards = res.data
@@ -25,6 +24,10 @@ angular.module('cardsOfKurskApp')
 
 
     $scope.pickFromTrade = function(userCardId, oldUserId) {
+      var loadingCardId = '#loading' + userCardId
+      console.log(loadingCardId)
+      angular.element( document.querySelector( loadingCardId ) ).addClass('loading')
+
       userService.getSessionId()
       .then(res => {
         const newUserId = res.data
@@ -33,14 +36,19 @@ angular.module('cardsOfKurskApp')
             cardService.pickFromTrade(userCardId, newUserId, oldUserId).then((res) => {
               if(!res.data){
                 toastr.error('You have no cards available for trading.', '0 tading cards!!');
-              }else socket.emit('updateAll')
+              }else {
+                  socket.emit('updateAll')
+                  angular.element( document.querySelector( loadingCardId ) ).removeClass('loading')
+                }
               })
           }
           else{
             toastr.warning('You are trying to get card that you yourself push.', 'That was yours!!')
+                  angular.element( document.querySelector( loadingCardId ) ).removeClass('loading')
           }
         }else{
           toastr.warning('You need to be loged in to pick cards!', 'Login!!')
+          angular.element( document.querySelector( loadingCardId ) ).removeClass('loading')
       }
 
       })
